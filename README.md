9b38511b6ce2e51d4a2e398092185a32e610abf2
Niranjan Jayakar <nija@databricks.com>
2023-03-01 10:28:55 +0100

    db connect

--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 
 Demonstration of using Databricks Connect V2 on Plotly.
 
+## Prerequisites
+
+You need to have the latest DB Connect Python tar file to run the application.
+If you don't have this, contact the authors - https://databricks.atlassian.net/wiki/spaces/UN/pages/2977858055/
+
 ## Running the app
 
 Setup and activate a Conda environment.
 
 ```shell
-conda env create -f conda.yml
+conda env create -f ./conda.yml
 conda activate dbconnect-plotly
+```
+
+Install DB Connect v2
+
+```shell
+pip install <dbconnect python tar>
 ```
 
 Run the plotly app.
--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from dash import Dash, html, dcc
 import plotly.express as px
-import pandas as pd
+from pyspark.sql.session import SparkSession
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import StringType
+
+def spark_session():
+    host = "<databricks workspace url>"
+    clusterId = "<cluster id>"
+    pat = "<token>"
+
+    connStr = f"sc://{host}:443/;token={pat};x-databricks-cluster-id={clusterId}"
+
+    return SparkSession.builder.remote(connStr).getOrCreate()
+
+def pickupzip_sample(spark: SparkSession):
+    df = spark.read.table("samples.nyctaxi.trips")
+    df = df.withColumn("pickup_zip", col("pickup_zip").cast(StringType()))
+    return df.groupby("pickup_zip").count().limit(5)
+
+
+spark = spark_session()
+sample = pickupzip_sample(spark).toPandas()
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.bar(sample, x="pickup_zip", y="count", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
